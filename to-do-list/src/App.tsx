b07a6c456f26df831c4d36f751d0359572b9bed5
vitordavips <vitordavidps@gmail.com>
2025-05-20 20:26:57 -0400

    criando Header e Footer

--- to-do-list/src/App.tsx
+++ to-do-list/src/App.tsx
@@ -1,12 +1,17 @@
-import { useState } from 'react'
 import './App.css'
+
+// components
+import Header from "./components/Header";
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
+      <Header/>
       <h1>To-do-List</h1>
+      <Footer/>
     </div>
   )
 }
 
-export default App
+export default App;
---
+++ to-do-list/src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+    return(
+        <footer>
+            <p>
+                <span>To-do-List</span> @2025
+            </p>
+        </footer>
+    )
+}
+
+
+export default Footer;
