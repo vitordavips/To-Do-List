0c9e49e5ab86c8f6d9ae8914dfd4972e1daaff8b
vitordavips <vitordavidps@gmail.com>
2025-05-20 22:11:27 -0400

    css form

--- to-do-list/src/components/TaksForm.tsx
+++ to-do-list/src/components/TaksForm.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
 
+//css
+import styles from "./TaksForm.module.css";
+
 interface Props {
     btnText: string
 }
 
 const TaksForm = ({btnText}: Props) => {
-    return <form>
-        <div>
+    return <form className={styles.form}>
+        <div className={styles.input_container}>
             <label htmlFor="title">Título</label>
             <input type="text" name='title' placeholder='Título da Tarefa'/>
         </div>
-        <div>
+        <div className={styles.input_container}>
             <label htmlFor="difficulty">Dificuldade:</label>
             <input type="text" name='difficulty' placeholder='Dificudade da tarefa'/>
         </div>
