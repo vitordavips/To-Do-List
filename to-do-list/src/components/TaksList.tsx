48cb8dd051adbd9f4ebe9b93768dcf19400baa68
vitordavips <vitordavidps@gmail.com>
2025-05-28 16:10:35 -0400

    finalizando a lista

--- to-do-list/src/components/TaksList.tsx
+++ to-do-list/src/components/TaksList.tsx
@@ -20,7 +20,7 @@ const TaksList = ({taskList}: Props) => {
               <h4>{task.title}</h4>
               <p>Dificuldade: {task.difficulty}</p>
             </div>
-            <div>
+            <div className={styles.actions}>
               <i className='bi bi-pencil'></i>
               <i className='bi bi-trash'></i>
             </div>
